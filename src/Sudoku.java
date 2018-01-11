5722d657c4cb06f8cb53bcada1a04d9091aaebeb
lg18 <lukas.gromer@uni-ulm.de>
2018-01-11 16:49:46 +0100

    Run Inspector Thread added

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,44 +1,67 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by lg18 on 11.01.2018.
  */
 public class Sudoku {
 
-    private int[][] sudoku;
-    private final int SIZE = 9;
-    private final int BOX_SIZE = 3;
+    private volatile int[][] sudoku;
+    private final int SIZE;
+    private final int BOX_SIZE;
+    volatile long steps = 0;
+    private volatile int xPos = 0;
+    private volatile int yPos = 0;
+    private RunInspector ri;
 
     public static void main(String[] args) {
-        Sudoku s = new Sudoku();
+        Sudoku s = new Sudoku(1);
+        System.out.println("Sudoku in Step " + s.steps);
         System.out.println(s);
     }
 
     public Sudoku() {
+        this(9);
+    }
+
+    public Sudoku(int size) {
+        int i;
+        for (i = 1; i * i < size; i++) ;
+        if (i * i == size) {
+            BOX_SIZE = i;
+            SIZE = size;
+        } else {
+            throw new IllegalArgumentException("Size must be a square but is " + size);
+        }
+        ri = new RunInspector(this);
         generateSudoku();
     }
 
     private void generateSudoku() {
         sudoku = new int[SIZE][SIZE];
-        int xPos = 0;
-        int yPos = 0;
-        while(xPos < 9 && yPos < 9) {
-            if(sudoku[yPos][xPos] == 9) {
+
+        new Timer().scheduleAtFixedRate(ri, 1000, 1000);
+        while (xPos < SIZE && yPos < SIZE) {
+            steps++;
+            if (sudoku[yPos][xPos] == SIZE) {
                 sudoku[yPos][xPos] = 0;
-                xPos = (xPos + 8) % 9;
-                if (xPos == 8) yPos--;
+                xPos = (xPos + SIZE - 1) % SIZE;
+                if (xPos == SIZE - 1) yPos--;
             } else {
                 boolean possible = insertAndTest(xPos, yPos, sudoku[yPos][xPos] + 1);
                 if (possible) {
-                    xPos = (xPos + 1) % 9;
+                    xPos = (xPos + 1) % SIZE;
                     if (xPos == 0) yPos++;
                 }
             }
         }
+        ri.cancel();
     }
 
     private boolean testRow(int xPos, int yPos) {
-        if(sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
-        for(int i = 0 ; i < SIZE ; i++) {
-            if(sudoku[yPos][xPos] == sudoku[yPos][i] && i != xPos) {
+        if (sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
+        for (int i = 0; i < SIZE; i++) {
+            if (sudoku[yPos][xPos] == sudoku[yPos][i] && i != xPos) {
                 return false;
             }
         }
@@ -46,9 +69,9 @@ public class Sudoku {
     }
 
     private boolean testColumn(int xPos, int yPos) {
-        if(sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
-        for(int i = 0 ; i < SIZE ; i++) {
-            if(sudoku[yPos][xPos] == sudoku[i][xPos] && i != yPos) {
+        if (sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
+        for (int i = 0; i < SIZE; i++) {
+            if (sudoku[yPos][xPos] == sudoku[i][xPos] && i != yPos) {
                 return false;
             }
         }
@@ -56,12 +79,12 @@ public class Sudoku {
     }
 
     private boolean testBox(int xPos, int yPos) {
-        if(sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
+        if (sudoku[yPos][xPos] == 0) throw new IllegalArgumentException("Tested Cell is not filled with a value!");
         int vNum = (yPos / BOX_SIZE) * BOX_SIZE;
         int hNum = (xPos / BOX_SIZE) * BOX_SIZE;
-        for(int i = vNum ; i < vNum + BOX_SIZE ; i++) {
-            for(int j = hNum ; j < hNum + BOX_SIZE ; j++) {
-                if(sudoku[yPos][xPos] == sudoku[i][j] && (i != yPos || j != xPos)) {
+        for (int i = vNum; i < vNum + BOX_SIZE; i++) {
+            for (int j = hNum; j < hNum + BOX_SIZE; j++) {
+                if (sudoku[yPos][xPos] == sudoku[i][j] && (i != yPos || j != xPos)) {
                     return false;
                 }
             }
@@ -72,11 +95,10 @@ public class Sudoku {
     private boolean insertAndTest(int xPos, int yPos, int value) {
         //System.out.println("testing at " + xPos + " " + yPos + " with Value " + value);
         sudoku[yPos][xPos] = value;
-        System.out.println(this.toString());
-        boolean possible =
-                testRow(xPos,yPos) &&
-                testColumn(xPos,yPos) &&
-                testBox(xPos,yPos);
+        //System.out.println(this.toString());
+        boolean possible = testRow(xPos, yPos) &&
+                        testColumn(xPos, yPos) &&
+                        testBox(xPos, yPos);
 
         return possible;
     }
@@ -84,12 +106,40 @@ public class Sudoku {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < SIZE ; i++) {
-            for(int j = 0 ; j < SIZE ; j++) {
-                sb.append(sudoku[i][j]).append(" ");
+        for (int i = 0; i < SIZE; i++) {
+            if (i % BOX_SIZE == 0) {
+
+                for (int j = 0; j < SIZE; j++) {
+                    if (j % BOX_SIZE == 0) {
+                        sb.append("+");
+                    }
+                    sb.append(" - ");
+                }
+                sb.append("\n");
             }
+            for (int j = 0; j < SIZE; j++) {
+                if (j % BOX_SIZE == 0) {
+                    sb.append("|");
+                }
+                sb.append(String.format("%2d", sudoku[i][j])).append(" ");
+            }
+
             sb.append("\n");
         }
         return sb.toString();
+    }
+}
+
+class RunInspector extends TimerTask {
+    Sudoku sudoku;
+
+    public RunInspector(Sudoku s) {
+        this.sudoku = s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Sudoku in Step " + sudoku.steps);
+        System.out.println(sudoku);
     }
 }
